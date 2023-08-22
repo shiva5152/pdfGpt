688565c21b05294a4499bb763c53d398a2cb0db7
shiva5152 <sg2304366@gmail.com>
2023-08-22 23:10:49 +0530

    change route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+"use client"
 import Image from 'next/image'
+import Loader from '@/components/Loder';
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+
+  const router=useRouter();
+  router.push('/chat');
+
+
   return (
-    <div>
-      hello
+    <div className='w-full flex justify-center items-center h-screen mx-auto'>
+     <Loader />
     </div>
   )
 }
